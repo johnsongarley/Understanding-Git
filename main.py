db7522e4e437212df121d989a8194c2dbedd347d
Johnson Garley <johnsongrear1@gmail.com>
2023-05-10 15:58:38 +1000

    second commit - just for my understanding

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 my_firstname = "Johnson"
 my_lastname = "Garley"
 full_name = my_firstname + " " + my_lastname
-print(full_name)
+
+This_year = 2023
+my_birth_year = 2001
+age = This_year - my_birth_year
+
+print("My name is " full_name + " age: " + my_birth_year + " of age.")
